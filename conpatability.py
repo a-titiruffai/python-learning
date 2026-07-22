df3c3b5b8db2a1eaa81e58322e38e581099a9b90
Titi Ruffai <titiruffai3@gmail.com>
2026-07-22 01:59:27 -0500

    Organizing Projects for GitHub

--- conpatability.py
+++
@@ -1,6 +0,0 @@
-import random
-print ("Hello, Welcome to the Compatibility Test!")
-Person_1 = input ("What is their name? ")
-Person_2 = input ("What is their name? ")
-print ("Hello", Person_1, "and", Person_2, "Compatability Loading....")
-print ("Your compatability score is", random.randint(0, 100))
--- hello2.py
+++
@@ -1,4 +0,0 @@
-print ("Hello PY4E World!")
-x = 5
-x = (x + 1)*2
-print (x)   
