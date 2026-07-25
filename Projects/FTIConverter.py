6d77f7adce5b21a9522e479bdcf3e4adb95fbf47
Titi Ruffai <titiruffai3@gmail.com>
2026-07-24 23:05:44 -0500

    Mini Project for the night before my bday

---
+++ Projects/FTIConverter.py
@@ -0,0 +1,8 @@
+nam = input ("Who Are You?")
+if nam == "Titi":
+    print ("Hello", nam, "This is your own personal feet to inches converter")
+    inp = input ( "How Many Feet?")
+    usf = int(inp) * 12
+    print ("This is the equivalent of", usf, "inches")
+else:
+    print ("Access Denied")
