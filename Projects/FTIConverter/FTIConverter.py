e7873e226ee196d4236b20260dea2d06583f718f
a-titiruffai <titiruffai3@gmail.com>
2026-07-28 23:41:13 -0500

    seperated locked vs unlocked fticonverter

---
+++ Projects/FTIConverter/FTIConverter.py
@@ -0,0 +1,5 @@
+nam = input ("Who Are You?")
+print ("Hello", nam, "This is your own personal feet to inches converter")
+inp = input ( "How Many Feet?")
+usf = int(inp) * 12
+print ("This is the equivalent of", usf, "inches")
